65f8077c8e360d28dca1e57abcdba15e4522c1d3
Stefan Einarsson <stefane81@gmail.com>
2019-01-27 13:30:29 +0100

    Switch between players

--- src/App.tsx
+++ src/App.tsx
@@ -3,39 +3,49 @@ import './App.css';
 
 export interface ISquareProps {
   value: string;
+  onClick: any;
 }
 
 export interface ISquareState {
   value: string;
+  onClick: any;
 }
 
-class Square extends React.Component<ISquareProps, ISquareState> {
-  constructor(props: ISquareState) {
-    super(props);
-    this.state = {
-      value: this.props.value,
-    };
-  }
+// class Square extends React.Component<ISquareProps, ISquareState> {
+//   constructor(props: ISquareState) {
+//     super(props);
+//   }
 
-  render() {
-    return (
-      <button className="square"
-        onClick={() => this.setState({ value: 'X' })}
-      >
-        {/* TODO */}
-        {this.state.value}
-      </button>
-    );
-  }
+//   render() {
+//     return (
+//       <button className="square"
+//         onClick={() => this.props.onClick()}
+//       >
+//         {/* TODO */}
+//         {this.props.value}
+//       </button>
+//     );
+//   }
+// }
+function Square(props: ISquareState) {
+  return (
+    <button className="square"
+      onClick={() => props.onClick()}
+    >
+      {/* TODO */}
+      {props.value}
+    </button>
+  );
 }
-
 
 export interface IBoardProps {
   squares: Array<any>;
+  xIsNext: boolean;
 }
 
 export interface IBoardState {
   squares: Array<any>;
+  xIsNext: boolean;
 }
 
 class Board extends React.Component<IBoardProps, IBoardState> {
@@ -44,14 +54,27 @@ class Board extends React.Component<IBoardProps, IBoardState> {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true,
     }
   }
   renderSquare(i: any) {
-    return <Square value={this.state.squares[i]} />;
+    return <Square
+      value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+    />;
+  }
+
+  handleClick(i: any) {
+    const squares = this.state.squares.slice();
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({ 
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   render() {
-    const status = 'Next player...: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X':'O');
 
     return (
       <div>
@@ -81,7 +104,7 @@ class App extends React.Component {
     return (
       <div className="game">
         <div className="game-board">
-          <Board squares={[]}/>
+          <Board squares={[]} xIsNext={true} />
         </div>
         <div className="game-info">
           <div>{/* status */}</div>
