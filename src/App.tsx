001344bb6d5df85b4aa31309f6070af488f7289e
Stefan Einarsson <stefane81@gmail.com>
2019-02-02 23:13:10 +0100

    Work in progress

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { object } from 'prop-types';
 
 export interface ISquareProps {
   value: string;
@@ -39,53 +40,25 @@ function Square(props: ISquareState) {
 }
 
 export interface IBoardProps {
-  squares: Array<any>;
-  xIsNext: boolean;
+  squares: Array<string>;
+  onClick: any;
 }
 
 export interface IBoardState {
-  squares: Array<any>;
-  xIsNext: boolean;
+  squares: Array<string>;
+  onClick: any;
 }
 
 class Board extends React.Component<IBoardProps, IBoardState> {
 
-  constructor(props: IBoardState) {
-    super(props);
-    this.state = {
-      squares: Array(9).fill(null),
-      xIsNext: true,
-    }
-  }
   renderSquare(i: any) {
     return <Square
-      value={this.state.squares[i]}
-      onClick={() => this.handleClick(i)}
+      value={this.props.squares[i]}
+      onClick={() => this.props.onClick(i)}
     />;
   }
 
-  handleClick(i: any) {
-    const squares = this.state.squares.slice();
-    if(calculateWinner(squares) || squares[i]){
-      return;
-    }
-    
-    squares[i] = this.state.xIsNext ? 'X' : 'O';
-    this.setState({
-      squares: squares,
-      xIsNext: !this.state.xIsNext,
-    });
-  }
-
   render() {
-    const winner = calculateWinner(this.state.squares);
-    let status;
-    if(winner){
-      status = 'Winner: ' + winner;
-    } else {
-      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
-    }
-
     return (
       <div>
         <div className="status">{status}</div>
@@ -109,12 +82,77 @@ class Board extends React.Component<IBoardProps, IBoardState> {
   }
 }
 
-class App extends React.Component {
+interface ISquares {
+  squares: any;
+}
+
+// type TSquaresObjects = {
+//   squares: Array<any>;
+// }
+
+interface IAppProps {  
+  history: Array<any>;
+  squares?: Array<any>;
+  xIsNext: boolean;
+}
+
+interface IAppState {
+  history: Array<any>;
+  squares?: Array<any>;
+  xIsNext: boolean;
+}
+
+class App extends React.Component<IAppProps, IAppState> {
+
+  constructor(props: IAppState){
+    super(props);
+    this.state = {
+      history: [{
+        squares: Array(9).fill(null),  
+      }],
+      
+      xIsNext: true,
+    }
+  }
+
+  handleClick(i: any) {
+
+    const history = this.state.history;
+    const current = history[history.length -1];
+    const squares = current.squares.slice();
+    if(calculateWinner(squares) || squares[i]){
+      console.log("winner");
+      return;
+    }
+
+    squares[i] = this.state.xIsNext ? 'X' : 'O';
+    this.setState({
+      history: history.concat([{
+        squares: squares,  
+      }]),
+      xIsNext: !this.state.xIsNext,
+    });
+  }
+
   render() {
+
+    const history = this.state.history;
+    const current = history[history.length -1];
+
+    const winner = calculateWinner(current.squares);
+    let status;
+    if(winner){
+      status = 'Winner: ' + winner;
+    } else {
+      status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
+    }
+
     return (
       <div className="game">
         <div className="game-board">
-          <Board squares={[]} xIsNext={true} />
+          <Board 
+          squares={[]} 
+          onClick={(i:any) => this.handleClick(i)} />
         </div>
         <div className="game-info">
           <div>{/* status */}</div>
@@ -133,8 +171,8 @@ function calculateWinner(squares: any) {
     [3, 4, 5],
     [6, 7, 8],
     [0, 3, 6],
-    [1, 4, 7],
     [2, 5, 8],
+    [1, 4, 7],
     [0, 4, 8],
     [2, 4, 6],
   ];
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Game from './App';
+import App from './App';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
-    <Game />,
+    <App 
+    history={[]}
+    xIsNext={true} 
+    
+    />,
     document.getElementById('root')
   );
 // If you want your app to work offline and load faster, you can change
